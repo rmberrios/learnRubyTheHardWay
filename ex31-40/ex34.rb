d86ccf682d3af9dd711e43591335f56d6d3cd000
Rommel Berrios <rommel.berrios93@gmail.com>
2016-01-24 21:29:12 -0600

    Add ex34 exercise
    Exercise 34 Accessing Elements of array

---
+++ ex31-40/ex34.rb
@@ -0,0 +1,18 @@
+# Exercise 34: Accessing Elements of Arrays
+# Grab from an array
+# 
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "Original array ", animals
+puts "-" * 30
+puts "The animal at 1. #{animals[1]}"
+puts "The third (3rd) animal. #{animals[2]}"
+puts "The first (1st) animal. #{animals[0]} "
+puts "The animal at 3. #{animals[3]}"
+puts "The fifth (5th) animal. #{animals[4]}"
+puts "The animal at 2. #{animals[2]}"
+puts "The sixth (6th) animal. #{animals[5]}"
+puts "The animal at 4. #{animals[4]}"
